2701382b0c19086043c81d47e9ab6ae3217c8cfb
Elostay <nastia60203@gmail.com>
2023-12-18 23:31:48 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,6 +6,7 @@ import ContactList from './ContactList/ContactList';
 import { Title, SubTitle, Container } from './ContactForm/ContactForm.styled';
 
 const App = () => {
+	
   return (
     <Container>
       <Title>Phonebook</Title>

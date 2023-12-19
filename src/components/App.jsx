8cd3e77a0a194b14a83373aca2892493e6efc6c1
Elostay <nastia60203@gmail.com>
2023-12-19 10:50:26 +0200

    add components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,17 +4,27 @@ import ContactForm from './ContactForm/ContactForm';
 import Filter from './Filter/Filter';
 import ContactList from './ContactList/ContactList';
 import { Title, SubTitle, Container } from './ContactForm/ContactForm.styled';
+import { Navigate, Route, Routes } from 'react-router-dom';
 
 const App = () => {
 	
-  return (
-    <Container>
-      <Title>Phonebook</Title>
-      <ContactForm />
-      <SubTitle>Contacts</SubTitle>
-      <Filter />
-      <ContactList />
-    </Container>
+	return (
+	  <>
+	  
+	  <Routes>
+				<Route path='/' element={}>
+
+				</Route>
+				<Route path='*' element={<Navigate to="/" />} />
+	  </Routes>
+	  </>
+   //  <Container>
+   //    <Title>Phonebook</Title>
+   //    <ContactForm />
+   //    <SubTitle>Contacts</SubTitle>
+   //    <Filter />
+   //    <ContactList />
+   //  </Container>
   );
 };
 
